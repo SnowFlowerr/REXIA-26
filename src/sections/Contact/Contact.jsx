969a556af7f31e14713cac558d072ec6ccea9264
Aditya Jindal <adityajindal2305@gmail.com>
2026-04-23 19:36:33 +0530

    fix redirect to yt link instead of instagram

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -103,7 +103,7 @@ const Contact = () => {
                     <circle cx="4" cy="4" r="2" />
                   </svg>
                 </a> */}
-                <a href="https://instagram.com/rexiafest/" target="_blank" className={styles.socialLink} aria-label="YouTube">
+                <a href="https://www.youtube.com/@Rexia26" target="_blank" className={styles.socialLink} aria-label="YouTube">
                   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                     <path d="M22.54 6.42a2.78 2.78 0 00-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 00-1.94 2A29 29 0 001 11.75a29 29 0 00.46 5.33A2.78 2.78 0 003.4 19.1c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 001.94-2 29 29 0 00.46-5.25 29 29 0 00-.46-5.43z" />
                     <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
